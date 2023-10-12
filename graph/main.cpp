85a0d1702293f3dd07cff05790ee9776a0812dac
Lev Droid <71851874+lev-droid@users.noreply.github.com>
2023-10-13 09:17:33 +1300

    add code commentes

--- graph/main.cpp
+++ graph/main.cpp
@@ -6,9 +6,10 @@
 #include "UI.hpp"
 #include "Algorithm.hpp"
 #include "Utility.hpp"
-
+// Function to get a node at a given position in the graph
 std::shared_ptr<Node> getNodeAtPosition(const Graph& graph, const sf::Vector2f& position) {
     for (const auto& node : graph.getNodes()) {
+        // Check if the distance between the node and the given position is less than or equal to the node's radius
         if (euclideanDistance(node->getPosition(), position) <= Node::NODE_RADIUS) {
             return node;
         }
@@ -16,48 +17,48 @@ std::shared_ptr<Node> getNodeAtPosition(const Graph& graph, const sf::Vector2f& 
     return nullptr;
 }
 
+// Function to update the weights of all edges in the graph based on the Euclidean distance between their start and end nodes
 void updateEdgeWeights(Graph& graph) {
     for (const auto& edge : graph.getEdges()) {
         float newWeight = euclideanDistance(edge->getStartNode()->getPosition(), edge->getEndNode()->getPosition());
         edge->setWeight(newWeight);
     }
-}    sf::Color currentNodeColor(255, 140, 0); // Orange
-    sf::Color processedNodeColor(191, 64, 191); // Light blue
+}    
+// Define colors for visualization
+sf::Color currentNodeColor(255, 140, 0); // Orange
+sf::Color processedNodeColor(191, 64, 191); // Light blue
     /// Remove unused variables and comments
     int main() {
+        // Create a window for the application
         sf::RenderWindow window(sf::VideoMode(800, 600), "Graph Shortest Path Visualizer");
         window.setFramerateLimit(60);
 
-
+        // Initialize graph, algorithm, and UI objects
         Graph graph;
         Algorithm algorithm(graph);
         UI ui(graph, algorithm, window);
 
-
+        // Variables to manage user interactions
         bool draggingNode = false, draggingEdge = false, stepByStepEnabled = false;
         UI::Mode prevMode = UI::Mode::NORMAL;
-
-
         std::shared_ptr<Node> selectedNode = nullptr, startNode = nullptr, endNode = nullptr;
         std::vector<std::vector<std::shared_ptr<Node>>> currentPaths;
         std::vector<std::pair<std::shared_ptr<Node>, std::shared_ptr<Node>>> steps;
         std::unordered_set<std::shared_ptr<Node>> processedNodes;
 
 
-
+        // Main loop to keep the window open
         while (window.isOpen()) {
             sf::Event event;
+            // Poll and handle events
             while (window.pollEvent(event)) {
                 ui.handleEvent(event, window);
-
-
-
-
+                // Close the window if the close event is triggered
                 if (event.type == sf::Event::Closed) {
                     window.close();
                 }
                 if (!ui.getSelectedButton()) {
-
+                    // Handle mouse and keyboard interactions if no UI button is selected
                     if (event.type == sf::Event::MouseButtonPressed) {
                         if (event.mouseButton.button == sf::Mouse::Left) {
                             sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Mouse::getPosition(window));
@@ -131,12 +132,13 @@ void updateEdgeWeights(Graph& graph) {
                     }
                 }
             }
+            // Handle node dragging
             if (draggingNode && selectedNode) {
                 sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Mouse::getPosition(window));
                 selectedNode->setPosition(mousePosition);
                 updateEdgeWeights(graph);
             }
-
+            // Clear the window for drawing
             window.clear();
             std::shared_ptr<Node> start = nullptr;
             std::shared_ptr<Node> goal = nullptr;
@@ -158,13 +160,15 @@ void updateEdgeWeights(Graph& graph) {
                         algorithm.renderPath(window); 
                     }
                 }
+
+                //draw the step by step path
                 else if (ui.getMode() == UI::Mode::STEP_BY_STEP) {
                     if (graph.getNodes().size() > 1) {
-                        algorithm.clearSteps();
+                        // reset the algorithm to rreset contstructed path
                         algorithm.clearSteps();
                         algorithm.resetPath();
                         algorithm.setCurrentPath(std::vector<std::shared_ptr<Node>>());
-
+                        //construct the path
                         DijkstraCallback callback = [&](std::shared_ptr<Node> currentNode, const std::vector<std::shared_ptr<Node>>& visitedNodes, const std::vector<std::shared_ptr<Node>>& neighbors, const std::vector<std::shared_ptr<Node>>& currentPath) {
                             if (!visitedNodes.empty()) {  // check if visitedNodes is not empty to avoid rendering an edge from a path already visited on previous iterations of the algorithm
                                 algorithm.addStep(std::make_pair(currentNode, neighbors.front()));
@@ -187,7 +191,7 @@ void updateEdgeWeights(Graph& graph) {
                         algorithm.renderStep(window, processedNodeColor, currentNodeColor);
                     }
                 }
-
+                //automated stepping
                 if (ui.getMode() == UI::Mode::AUTO_STEP_BY_STEP) {
                     std::cout << "In AUTO_STEP_BY_STEP mode" << std::endl; // Debug print
                     if (ui.getAutoStepClock().getElapsedTime().asSeconds() >= ui.getAutoStepInterval()) {
@@ -202,8 +206,9 @@ void updateEdgeWeights(Graph& graph) {
                 }
 
             }
-
+            // Draw the UI elements
             ui.draw(window);
+            // Display the rendered frame
             window.display();
         }
 
